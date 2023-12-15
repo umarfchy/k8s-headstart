083a19d9e5d0cc5c06d0b7948b84f0da3928e1e5
umarfchy <umarfchy@gmail.com>
2023-12-16 01:34:09 +0600

    working api with k8s manifest

---
+++ lesson-02-deploy-backend/api/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import os from "os";
+
+const HOSTNAME = os.hostname();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_, res) => res.send("hello!!!"));
+app.post("/", (req, res) => {
+  try {
+    eventEmitter.emit("start", req.body);
+    res.status(200).json({
+      host: HOSTNAME,
+      message: "success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      host: HOSTNAME,
+      message: "failed",
+    });
+  }
+});
+
+app.listen(PORT, () => console.log("api server started on port", PORT));
